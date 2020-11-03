5c2228fdbf9f03087870dc2ff33e9901b2d5ea21
karth-shen@hotmail.com <karth-shen@hotmail.com>
2020-11-02 21:08:16 -0500

    Added battle VFX

---
+++ Assets/Scripts/Attack/MoveProjectile.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveProjectile : MonoBehaviour
+{
+    public float speed;
+    public float fireRate;
+    public GameObject muzzlePrefab;
+    public GameObject hitPrefab;
+
+
+    void Start()
+    {
+        if(muzzlePrefab != null)
+        {
+            var muzzleVFX = Instantiate(muzzlePrefab, transform.position, Quaternion.identity);
+            var pMuzzle = muzzleVFX.GetComponent<ParticleSystem>();
+            if (pMuzzle != null)
+                Destroy(muzzleVFX, pMuzzle.main.duration);
+            else
+            {
+                var pChild = muzzleVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(muzzleVFX, pChild.main.duration);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (speed != 0)
+        {
+            transform.position += transform.forward * (speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        speed = 0;
+
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+
+        if(hitPrefab != null)
+        {
+            var hitVFX = Instantiate(hitPrefab, pos, rot);
+            SoundManager.PlaySound("atkHit1");
+            var pHit = hitVFX.GetComponent<ParticleSystem>();
+            if (pHit != null)
+                Destroy(hitVFX, pHit.main.duration);
+            else
+            {
+                var pChild = hitVFX.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(hitVFX, pChild.main.duration);
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Attack/SpawnProjectile.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectile : MonoBehaviour
+{
+    public GameObject firePoint;
+    //public List<GameObject> vfx = new List<GameObject>();
+    public GameObject basicAttack, specialAttack;
+    GameObject laser;
+
+    void Start()
+    {
+        laser = Instantiate(specialAttack, firePoint.transform.position, Quaternion.identity);
+        laser.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (firePoint != null)
+            {
+                Instantiate(basicAttack, firePoint.transform.position, Quaternion.identity);
+                SoundManager.audioSrc.volume = 0.3f;
+                SoundManager.PlaySound("atk1");
+            }
+        }
+
+        //Laser Attack on right mouse button down
+        if (Input.GetMouseButton(1))
+        {
+            if (firePoint != null)
+            {
+                laser.SetActive(true);
+            }
+
+            //Play sound
+            if (!SoundManager.audioSrc.isPlaying)
+            {
+                SoundManager.PlaySound("atk2");
+                SoundManager.audioSrc.loop = true;
+            }
+        }
+
+        // Hide laser VFX on right mouse button up
+        if (Input.GetMouseButtonUp(1))
+        {
+            SoundManager.audioSrc.Stop();
+            laser.SetActive(false);
+
+        }
+
+    }
+}
