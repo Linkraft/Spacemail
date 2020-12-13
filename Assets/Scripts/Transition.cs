fb9bbbc6847e8245c346c55f03554435a4f07e4a
karth-shen@hotmail.com <karth-shen@hotmail.com>
2020-12-13 00:31:18 -0500

    Added transition scene

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+    public Light flashLight;
+    public GameObject ray;
+    Animator animator;
+    public int lightStep = 10;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        ray.SetActive(false);
+        StartCoroutine(ExecuteAfterTime(0.9f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LightOff();    
+    }
+
+    void LightOff()
+    {
+        flashLight.intensity -= lightStep * Time.deltaTime;
+        if ( flashLight.intensity <8 && flashLight.intensity>0)
+        {
+            ShowRays();
+        }
+    }
+
+    void ShowRays()
+    {
+        ray.SetActive(true);
+    }
+
+    void MoveShip()
+    {
+        animator.SetBool("MoveTo", true);
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        MoveShip();
+    }
+}
