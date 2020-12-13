84eef27189a8ba56d6d87cc5a97d4bb7ad2f5821
Smith <linkraft@ufl.edu>
2020-12-13 15:04:54 -0500

    Menu blocked out

---
+++ Assets/Scripts/VRButtonCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRButtonCollider : MonoBehaviour
+{
+    Color initSpriteColor;
+    public Color newSpriteColor;
+
+    private void Start()
+    {
+        initSpriteColor = GetComponentInParent<SpriteRenderer>().color;   
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter called on " + gameObject.name);
+        GetComponentInParent<SpriteRenderer>().color = newSpriteColor;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("OnTriggerExit called on " + gameObject.name);
+        GetComponentInParent<SpriteRenderer>().color = initSpriteColor;
+    }
+}
