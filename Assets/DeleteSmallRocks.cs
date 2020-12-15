1ef226733f2b4a0ce31e3dd46f14562d05b95380
patrickmolen <35581599+patrickmolen@users.noreply.github.com>
2020-12-15 13:39:53 -0500

    Rocks Delete and clouds

---
+++ Assets/DeleteSmallRocks.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteSmallRocks : MonoBehaviour
+{
+    Transform[] childrenObjects;
+    float timeInstantiated;
+    ObstacleMovment wow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(KillRocks());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //StartCoroutine(KillRocks());
+
+
+    }
+
+    IEnumerator KillRocks()
+    {
+        yield return new WaitForSeconds(10);
+        Destroy(this.gameObject);
+
+    }
+}
